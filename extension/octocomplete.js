b0ef18935c8801edc91261ba47a59a4d5a802cf4
Ed Rooth <ed.rooth@gmail.com>
2012-09-23 20:24:16 -0700

    embedding icons in the menu. updated todos

--- extension/octocomplete.js
+++ extension/octocomplete.js
@@ -5,8 +5,11 @@
       DATA_ENDPOINT = 'http://localhost:3000/?q=http://www.emoji-cheat-sheet.com',
       IMG_PATH = 'http://www.emoji-cheat-sheet.com/graphics/emojis/',
       menuVisible = false,
-      menuEl = null;
+      menuEl = null,
+      emoji = null;
 
+  // TODO: use github's CDN images?
+  // TODO: cache these
   function fetchEmoji(cb) {
     var xhr = new XMLHttpRequest();
     console.log('start fetch');
@@ -14,7 +17,6 @@
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4) {
         window.resp = xhr.response;
-        //console.log(xhr.response);
         cb(xhr.response);
       }
     };
@@ -22,9 +24,9 @@
     console.log('end fetch');
   }
 
-  // remove uneeded junk prior to creating dom element
+  // TODO: strip out script tags, images, css, etc.
+  // and any other uneeded junk prior to creating dom element
   function scrubDocString(docString) {
-    // TODO: strip out script tags, images, css, etc.
     var newString = docString;
     return newString;
   }
@@ -41,6 +43,7 @@
         .replace('.png', '');
     });
     console.log(emojiNames);
+    emoji = emojiNames.sort();
   }
 
   // walk up the tree applying func to each el.prop
@@ -68,19 +71,41 @@
     return offset;
   }
 
+  function filterEmoji(search) {
+    // TODO: make this work
+    return emoji;
+  }
+
+  function getImgUrl(emojiName) {
+    return IMG_PATH + emojiName + '.png';
+  }
+
+  function getImgTag(emojiName) {
+    return '<img src="' + getImgUrl(emojiName) + '"/>';
+  }
+
+  function populateMenu() {
+    var ul = document.createElement('ul'),
+        listItemHtml = '';
+    menuEl.innerHTML = '';
+    emoji.forEach(function (e) {
+      listItemHtml += '<li>' + getImgTag(e) + ':' + e + ':</li>';
+    });
+    ul.innerHTML = listItemHtml;
+    menuEl.appendChild(ul);
+  }
+
+  // TODO: if not visible, scroll into view.
+  // TOTO: populate menu with matching names
   function showMenu(targetEl) {
     var cursorPosition = targetEl.selectionStart,
         fontSize = parseInt(window.getComputedStyle(targetEl)['font-size'], 10),
-        //topPos = document.body.scrollTop + targetEl.offsetTop + parseInt(fontSize, 10),
         topPos = getOffsetTop(targetEl) + targetEl.offsetHeight,
         leftPos = getOffsetLeft(targetEl);
 
-    console.log('textposition:' + targetEl.selectionStart);
-    console.log('left:' + leftPos);
-    console.log('top:' + topPos);
+    populateMenu();
     menuEl.style.top = topPos + 'px';
     menuEl.style.left = leftPos + 'px';
-    console.log('el top: ' + menuEl.style.top);
   }
 
   function hideMenu() {
@@ -90,7 +115,7 @@
   }
 
   function initMenu() {
-    menuEl = window.document.createElement('div');
+    menuEl = document.createElement('div');
     menuEl.setAttribute('class', 'octo-complete-menu');
     menuEl.innerText = 'thisis a menu';
     document.body.appendChild(menuEl);
@@ -109,6 +134,7 @@
     }
   }
 
+  // TODO: hide menu on any type of click or textarea blur
   function setupListeners() {
     document.addEventListener('keypress', handleKeypress, false);
   }
