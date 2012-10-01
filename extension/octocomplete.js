d184796b4b651bdff92cb9a81254e372883c10a0
Ed Rooth <ed.rooth@gmail.com>
2012-09-30 17:06:56 -0700

    some refactoring and structural cleanup

--- extension/octocomplete.js
+++ extension/octocomplete.js
@@ -1,146 +1,185 @@
 (function () {
 'use strict';
 
-  var store = window.localStorage,
-      DATA_ENDPOINT = 'http://localhost:3000/?q=http://www.emoji-cheat-sheet.com',
-      IMG_PATH = 'http://www.emoji-cheat-sheet.com/graphics/emojis/',
-      menuVisible = false,
-      menuEl = null,
-      emoji = null;
+  var root = this,
+  doc = root.document,
+  store = root.localStorage,
+  DATA_ENDPOINT = 'http://localhost:3000/?q=http://www.emoji-cheat-sheet.com',
+  IMG_PATH = 'https://a248.e.akamai.net/assets.github.com/images/icons/emoji/',
+  instance,
+  OctoComplete = function () {
+    this.menuVisible = false;
+    this.menuEl = null;
+    this.isVisible = false;
+    this.emojiList = store.emojiList || null;
+  };
 
-  // TODO: use github's CDN images?
-  // TODO: cache these
-  function fetchEmoji(cb) {
-    var xhr = new XMLHttpRequest();
-    console.log('start fetch');
-    xhr.open('GET', DATA_ENDPOINT, true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4) {
-        window.resp = xhr.response;
-        cb(xhr.response);
+  OctoComplete.prototype = {
+    init: function () {
+      this.fetch(this.onFetch.bind(this));
+      this.injectMenu();
+      this.attachListeners();
+    },
+
+    // TODO: hide menu on any type of click or textarea blur
+    attachListeners: function () {
+      doc.addEventListener('keypress', this.onKeypress.bind(this), false);
+      doc.addEventListener('click', this.onClick.bind(this), false);
+    },
+
+    injectMenu: function () {
+      var menuEl = this.menuEl = doc.createElement('div');
+      menuEl.setAttribute('class', 'octo-complete-menu');
+      menuEl.innerText = 'thisis a menu';
+      doc.body.appendChild(menuEl);
+    },
+
+    // TODO: use github's CDN images?
+    // TODO: cache these
+    fetch: function (callback, errback) {
+      // if exists in local storage
+      // ...
+      // else
+      var xhr = new root.XMLHttpRequest();
+      xhr.open('GET', DATA_ENDPOINT, true);
+      xhr.onreadystatechange = function() {
+        // TODO: call errback() if error
+        if (xhr.readyState === 4) {
+          callback(xhr.response);
+        }
+      };
+      xhr.send();
+    },
+
+    scrape: function (htmlString) {
+      var tmpEl = doc.createElement('div'),
+          emojiNodes,
+          emojiNames;
+      tmpEl.innerHTML = this.scrubHtmlString(htmlString);
+      emojiNodes = tmpEl.querySelectorAll('.emoji');
+      emojiNames = Array.prototype.map.call(emojiNodes, function (emojiNode) {
+        return emojiNode.getAttribute('data-src')
+          .replace('graphics/emojis/', '')
+          .replace('.png', '');
+      });
+      return emojiNames.sort();
+    },
+
+    // TODO: if not visible, scroll into view.
+    // TOTO: populate menu with matching names
+    show: function (targetEl) {
+      var cursorPosition = targetEl.selectionStart,
+          fontSize = parseInt(root.getComputedStyle(targetEl)['font-size'], 10),
+          topPos = this.calcOffsetTop(targetEl) + targetEl.offsetHeight,
+          leftPos = this.calcOffsetLeft(targetEl);
+
+      this.populateMenu();
+      this.menuEl.style.top = topPos + 'px';
+      this.menuEl.style.left = leftPos + 'px';
+      this.menuEl.style.display = 'block';
+      this.isVisible = true;
+    },
+
+    hide: function() {
+      this.menuEl.style.display = 'none';
+      this.isVisible = false;
+    },
+
+    select: function (content) {
+      console.log('selected: ' + content);
+    },
+
+    // walk up the tree applying func to each el.prop
+    walkTree: function (el, func) {
+      var parentEl = el;
+      while (parentEl) {
+        func(parentEl);
+        parentEl = parentEl.offsetParent;
       }
-    };
-    xhr.send();
-    console.log('end fetch');
-  }
+    },
 
-  // TODO: strip out script tags, images, css, etc.
-  // and any other uneeded junk prior to creating dom element
-  function scrubDocString(docString) {
-    var newString = docString;
-    return newString;
-  }
+    calcOffsetLeft: function (el) {
+      var offset = 0;
+      this.walkTree(el, function (parentEl) {
+        offset += parentEl.offsetLeft;
+      });
+      return offset;
+    },
 
-  function scrape(docString) {
-    var el = document.createElement('div'),
-        emojiNodes,
-        emojiNames;
-    el.innerHTML = scrubDocString(docString);
-    emojiNodes = el.querySelectorAll('.emoji');
-    emojiNames = Array.prototype.map.call(emojiNodes, function (emojiNode) {
-      return emojiNode.getAttribute('data-src')
-        .replace('graphics/emojis/', '')
-        .replace('.png', '');
-    });
-    console.log(emojiNames);
-    emoji = emojiNames.sort();
-  }
+    calcOffsetTop: function (el) {
+      var offset = 0;
+      this.walkTree(el, function (parentEl) {
+        offset += parentEl.offsetTop;
+      });
+      return offset;
+    },
 
-  // walk up the tree applying func to each el.prop
-  function walkTree(el, func) {
-    var parentEl = el;
-    while (parentEl) {
-      func(parentEl);
-      parentEl = parentEl.offsetParent;
-    }
-  }
-
-  function getOffsetLeft(el) {
-    var offset = 0;
-    walkTree(el, function (parentEl) {
-      offset += parentEl.offsetLeft;
-    });
-    return offset;
-  }
-
-  function getOffsetTop(el) {
-    var offset = 0;
-    walkTree(el, function (parentEl) {
-      offset += parentEl.offsetTop;
-    });
-    return offset;
-  }
-
-  function filterEmoji(search) {
     // TODO: make this work
-    return emoji;
-  }
+    filter: function (search) {
+      return this.emojiList;
+    },
 
-  function getImgUrl(emojiName) {
-    return IMG_PATH + emojiName + '.png';
-  }
+    buildImgUrl: function (emojiName) {
+      return IMG_PATH + emojiName + '.png';
+    },
 
-  function getImgTag(emojiName) {
-    return '<img src="' + getImgUrl(emojiName) + '"/>';
-  }
+    buildImgTag: function (emojiName) {
+      return '<img src="' + this.buildImgUrl(emojiName) + '"/>';
+    },
 
-  function populateMenu() {
-    var ul = document.createElement('ul'),
-        listItemHtml = '';
-    menuEl.innerHTML = '';
-    emoji.forEach(function (e) {
-      listItemHtml += '<li>' + getImgTag(e) + ':' + e + ':</li>';
-    });
-    ul.innerHTML = listItemHtml;
-    menuEl.appendChild(ul);
-  }
+    populateMenu: function () {
+      var ul = doc.createElement('ul'),
+          listItemHtml = '';
+      this.menuEl.innerHTML = '';
+      this.emojiList.forEach(function (e) {
+        listItemHtml += '<li>' + this.buildImgTag(e) + ':' + e + ':</li>';
+      }, this);
+      ul.innerHTML = listItemHtml;
+      this.menuEl.appendChild(ul);
+    },
 
-  // TODO: if not visible, scroll into view.
-  // TOTO: populate menu with matching names
-  function showMenu(targetEl) {
-    var cursorPosition = targetEl.selectionStart,
-        fontSize = parseInt(window.getComputedStyle(targetEl)['font-size'], 10),
-        topPos = getOffsetTop(targetEl) + targetEl.offsetHeight,
-        leftPos = getOffsetLeft(targetEl);
+    // TODO: strip out script tags, images, css, etc.
+    // and any other uneeded junk prior to creating dom element
+    scrubHtmlString: function (htmlString) {
+      var newString = htmlString;
+      return newString;
+    },
 
-    populateMenu();
-    menuEl.style.top = topPos + 'px';
-    menuEl.style.left = leftPos + 'px';
-  }
+    onFetch: function (response) {
+      this.emojiList = this.scrape(response);
+      // TODO: show message 'fetch complete'
+    },
 
-  function hideMenu() {
-  }
-
-  function makeSelection(text) {
-  }
-
-  function initMenu() {
-    menuEl = document.createElement('div');
-    menuEl.setAttribute('class', 'octo-complete-menu');
-    menuEl.innerText = 'thisis a menu';
-    document.body.appendChild(menuEl);
-  }
-
-  function handleKeypress(e) {
-    var target = e.target;
-    if (target.nodeName !== 'TEXTAREA') {
-      return;
-    }
-    if (e.keyCode === 58) {
-      if (!menuVisible) {
-        //menuVisible = 'show-menu';
-        showMenu(target);
+    onKeypress: function (e) {
+      var target = e.target;
+      if (target.nodeName !== 'TEXTAREA') {
+        return;
       }
+      if (e.keyCode === 58) {
+        if (!this.menuVisible) {
+          //menuVisible = 'show-menu';
+          this.show(target);
+        }
+      }
+    },
+
+    onClick: function (e) {
+      var target = e.target;
+      if (!this.isVisible) {
+        return;
+      }
+      // TODO: or not a descendant
+      if (target.parentElement &&
+          target.parentElement.parentElement === this.menuEl) {
+        // TODO: pass inner content
+        this.select(target.textContent);
+      }
+      this.hide();
     }
-  }
 
-  // TODO: hide menu on any type of click or textarea blur
-  function setupListeners() {
-    document.addEventListener('keypress', handleKeypress, false);
-  }
+  };
 
-  fetchEmoji(scrape);
-  initMenu();
-  setupListeners();
+  instance = new OctoComplete();
+  instance.init();
 
-}());
+}).call(this);
